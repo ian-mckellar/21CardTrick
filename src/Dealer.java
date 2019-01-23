5feed3ad04b9598aec4863e4cf9058d5f4e1f43e
carsontyler <17ctyler@gmail.com>
2019-01-23 11:06:16 -0700

    Added Constructors
    Dealer and Deck constructors

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,15 +1,22 @@
 public class Dealer {
     private int dealNumber;
+    private Deck deck;
+
+    public Dealer() {
+        deck = new Deck();
+        dealNumber = 0;
+    }
 
     public void Deal() {
-
+        dealNumber++;
+        
     }
 
     public void RevealCard() {
-
+        
     }
 
-    public void pickupCards() {
+    public void PickupCards() {
         
     }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> deck;
+    private static final String[] suit = {"Hearts", "Diamonds", "Spades", "Clovers"}; 
+    private static final String[] face = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+
+    public Deck() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                Card card = new Card(suit[i], face[j]);
+                Add(card);
+            }        
+        }
+        Shuffle(); 
+        Random21();
+    }
+
     public void Shuffle() {
 
     }
