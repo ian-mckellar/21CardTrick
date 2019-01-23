b991d2c92c3500beafb844fbee2f6467058c6b8d
ian <ian.douglas.mckellar@gmail.com>
2019-01-23 11:03:11 -0700

    card and deck classes

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,66 @@
-public class Card {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class Card extends JPanel {
+    private String suit;
+    private String face;
+    private String imageName;
+    private ImageIcon cardImage;
+
+    /**
+     * initialise a card with a suit(spade, heart, diamond, club) and a face(jack, queen, king)
+     * all face cards have a value of 10
+     *
+     * @param suit - spade, heart, diamond, club
+     * @param face - ace, jack, queen, kind
+     */
+    Card(String face, String suit, String imageName) {
+        this.face = face;
+        this.suit = suit;
+        this.imageName = imageName;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawCard(g);
+    }
+
+    private void drawCard(Graphics g) {
+        ImageIcon imageIcon = new ImageIcon();
+        imageIcon.paintIcon(this, g, 25, 5);
+    }
+
+    /**
+     * @return String representing suit of the card
+     */
+    String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @return String representing face of card
+     */
+    String getFace() {
+        return face;
+    }
+
+    /**
+     * @return String repreenting location of image in project
+     */
+    String getImageName() {
+        return imageName;
+    }
+
+    /**
+     * returns string with field values for cards for testing purposes
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return String.format("suit = %-8s " +
+                "face = %-5s " +
+                "imageName = %10s", suit, face, imageName);
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
-    
+    /**
+     * The location of the top card. All cards before this position are in use or
+     * have been discarded. From this position on is the deck.
+     */
+    private int topCardLocation = 0;
+    private ArrayList<Card> cardArrayList = new ArrayList<>();
+
+    ArrayList<Card> getCardArrayList() {
+        return cardArrayList;
+    }
+
+    /**
+     * Fills cardArrayList with one of each card
+     */
+    public void fillDeck() {
+        String[] faceValues = {"ace", "jack", "queen", "king"};
+        String[] suits = {"clubs", "diamonds", "hearts", "spades"};
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                cardArrayList.add(new Card(faceValues[i], suits[j],
+                        "images/" + faceValues[i] + "_of_" + suits[j] + ".png"));
+            }
+        }
+    }
+
+    /**
+     * Shuffles the deck
+     */
+    public void shuffleDeck() {
+        topCardLocation = 0;
+        Collections.shuffle(cardArrayList);
+    }
+
+    /**
+     * Simulates adding a new deck
+     */
+    public void addNewDeck() {
+        shuffleDeck();
+    }
 }
